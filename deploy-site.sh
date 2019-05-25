03d40f81db56318c7131afb9da98cb2254f0b60c
colobas <mail@gpir.es>
2019-05-24 17:07:39 -0700

    commented deploy script

--- deploy-site.sh
+++ deploy-site.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/sh
 
-nikola build && ghp-import --cname="gpir.es" output && git push git@github.com:colobas/colobas.github.io.git gh-pages:master
+nikola build
+
+# the final website code goes to the gh-pages branch in this repo
+ghp-import --cname="gpir.es" output 
+
+# push from the gh-pages branch of this repo
+# to the master branch on the real website repo
+git push git@github.com:colobas/colobas.github.io.git gh-pages:master
 
